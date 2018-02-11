43c515a084c26bb9fbea7047548fd15da976a092
puffyboa <julian@sankergroup.org>
2018-02-11 13:36:33 -0600

    now it will hopefully nto error on some computers

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ import os
 
 
 def retrieveIP():
-    ip = (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
+    try:
+        ifconfig = os.popen("ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'").read()
+        ip = ifconfig.split("\n")[0]
+    except:
+        ip = (([ip for ip in socket.gethostbyname_ex(socket.gethostname())[2] if not ip.startswith("127.")] or [[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
     return ip
 
 
