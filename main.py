d83e79ed7516b84e47234802d9dbc89977469fc4
puffyboa <julian@sankergroup.org>
2018-02-12 19:54:44 -0600

    now it works

--- main.py
+++ main.py
@@ -221,7 +221,10 @@ def get_bcast_ip():
 def list_netips():
     selector = re.compile("^\? \({}\)".format(re_ip))
     broadcast_ip = get_bcast_ip()
-    subprocess.check_output(["ping","-i","0.1","-c","2",broadcast_ip])
+    try:
+        subprocess.check_output(["ping","-i","0.1","-c","2",broadcast_ip])
+    except subprocess.CalledProcessError:
+        subprocess.check_output(["ping", "-b", "-i", "0.1", "-c", "2", broadcast_ip])
 
     arps = subprocess.check_output(["arp", "-a"]).decode()
     ips = [re.match(selector, a) for a in arps.split('\n')]
